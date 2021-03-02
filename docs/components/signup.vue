22fa2b8223277fcd2bd44a622d7ad667e4600da3
jacobhq <jacob.marshall.jem@gmail.com>
2021-03-02 17:25:06 +0000

    Specify list

--- docs/components/signup.vue
+++ docs/components/signup.vue
@@ -1,15 +1,13 @@
 <template>
   <!-- Begin Mailchimp Signup Form -->
 <div id="mc_embed_signup">
-<v-form action="https://mail.jacobhq.xyz/subscription/form" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+<v-form action="https://mail.jacobhq.xyz/subscription/form" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
 	  <h1>Subscribe</h1>
-    <small class="grey--text text--darken-1" style="margin-top: 5px">None of this data is shared with anyone.</small>
-    <v-text-field label="Email Address" name="email" v-model="email" :rules="emailRules" required></v-text-field>
-    <v-text-field label="First name" name="name" v-model="firstname" :rules="nameRules" :counter="10" required></v-text-field>
-    <p>
-          <input id="8d933" type="checkbox" name="l" value="8d9335ad-cdba-4018-9fd8-4e08e07485ac" />
-          <label for="8d933">Post</label>
-        </p>
+    <small class="grey--text text--darken-1" style="margin-top: 5px">None of this data is shared with anyone, and it is only used to send you email.</small>
+    <br>
+    <v-text-field label="Email Address *" name="email" v-model="email" :rules="emailRules" required></v-text-field>
+    <v-text-field label="First name *" name="name" v-model="firstname" :rules="nameRules" :counter="25" required></v-text-field>
+    <input id="8d933" type="checkbox" name="l" value="8d9335ad-cdba-4018-9fd8-4e08e07485ac" v-model="post" style="display: none;" />
     <div style="margin-top: 10px;">
       <v-btn depressed color="primary" type="submit" value="Subscribe" name="subscribe">Signup</v-btn>
       <v-btn text color="primary" @click="sub" :loading="loading" :disabled="loading">Go home</v-btn>
@@ -30,11 +28,12 @@ export default {
       loader: null,
       loading: false,
       valid: false,
+      post: true,
     firstname: '',
     lastname: '',
     nameRules: [
       v => !!v || 'Name is required',
-      v => v.length <= 10 || 'Name must be less than 10 characters',
+      v => v.length <= 25 || 'Name must be less than 25 characters',
     ],
     email: '',
     emailRules: [
