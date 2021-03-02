a4ee737c3f3a662d7ca51cd4e9e4cd3c86f49f5c
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-03-02 13:10:42 +0000

    Use listmonk

--- docs/components/signup.vue
+++ docs/components/signup.vue
@@ -1,13 +1,15 @@
 <template>
   <!-- Begin Mailchimp Signup Form -->
 <div id="mc_embed_signup">
-<v-form action="https://anchor.us17.list-manage.com/subscribe/post?u=22c14fa2c4ccc79555a27ecd1&amp;id=addfa88c25" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+<v-form action="https://mail.jacobhq.xyz/subscription/form" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
 	  <h1>Subscribe</h1>
     <small class="grey--text text--darken-1" style="margin-top: 5px">None of this data is shared with anyone.</small>
-    <v-text-field label="Email Address" name="EMAIL" v-model="email" :rules="emailRules" required></v-text-field>
-    <v-text-field label="First name" name="FNAME" v-model="firstname" :rules="nameRules" :counter="10" required></v-text-field>
-    <v-text-field label="Last name" name="LNAME" v-model="lastname" :rules="nameRules" :counter="10"></v-text-field>
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_22c14fa2c4ccc79555a27ecd1_addfa88c25" tabindex="-1" value=""></div>
+    <v-text-field label="Email Address" name="email" v-model="email" :rules="emailRules" required></v-text-field>
+    <v-text-field label="First name" name="name" v-model="firstname" :rules="nameRules" :counter="10" required></v-text-field>
+    <p>
+          <input id="8d933" type="checkbox" name="l" value="8d9335ad-cdba-4018-9fd8-4e08e07485ac" />
+          <label for="8d933">Post</label>
+        </p>
     <div style="margin-top: 10px;">
       <v-btn depressed color="primary" type="submit" value="Subscribe" name="subscribe">Signup</v-btn>
       <v-btn text color="primary" @click="sub" :loading="loading" :disabled="loading">Go home</v-btn>
